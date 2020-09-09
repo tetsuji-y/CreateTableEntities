c11316591f0aa725676c0111e15819e9855990a0
tetsuji-y <yabuki.mobile@gmail.com>
2020-09-09 18:08:44 +0900

    Add files via upload

---
+++ CreateTableEntities/CreateTableEntities/EntityBuilder.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CreateTableEntities
+{
+    /// <summary>
+    /// Entity（Dto）用テキスト生成ビルダークラス
+    /// </summary>
+    public class EntityBuilder
+    {
+        public string TableName { get; set; }
+
+        private List<(string DataType, string Field)> Fields { get; } = new List<(string, string)>();
+
+        public void AddTable(string tableName)
+        {
+            var master = string.Empty;
+
+            if ("m".Equals(tableName.Substring(0, 1)))
+                master = "Master";
+ 
+           this.TableName = $"{Regex.Replace(tableName, @"^[mt]_", string.Empty).ToPascalCase()}{master}";
+        }
+
+        public void AddField(string dataType, string fieldName)
+            => this.Fields.Add((DataType:dataType, Field:fieldName));
+
+        public string Build()
+        {
+            return
+$@"using System;
+
+namespace XXXX //「XXXX」部分については機能毎の名称をセットしてください
+{{
+    public class {this.TableName} 
+    {{
+{BuildFields()}
+    }}
+}}";
+        }
+
+        private string BuildFields()
+        {
+            var sb = new StringBuilder();
+
+            foreach(var f in this.Fields)
+            {
+                sb.AppendLine($"        public {ToCSharpDataType(f.DataType)} {f.Field.ToPascalCase()} {{ get; set; }}");
+            }
+
+            return sb.ToString();
+        }
+
+        private string ToCSharpDataType(string dataType)
+        {
+            //参照： https://docs.microsoft.com/ja-jp/dotnet/framework/data/adonet/sql-server-data-type-mappings
+
+            switch (dataType)
+            {
+                case "smallint":
+                case "int":
+                    return "int";
+                case "bigint":
+                    return "long";
+                case "nchar":
+                case "varchar":
+                case "nvarchar":
+                    return "string";
+                case "date":
+                case "datetime":
+                case "datetime2":
+                    return nameof(DateTime);
+                case "decimal":
+                    return "decimal";
+                case "float":
+                    return "double";
+                default:
+                    return "object";
+            }
+        }
+    }
+
+    internal static class TableNameExtension
+    {
+        public static string ToPascalCase(this string s)
+        {
+            var sb = new StringBuilder();
+            var chars = s.ToCharArray();
+            var IsUppder = false;
+
+            sb.Append(chars[0].ToString().ToUpper());
+            
+            for (int i = 1; i < chars.Length; i++)
+            {
+                switch (chars[i])
+                {
+                    case '_':
+                        IsUppder = true;
+                        break;
+                    default:
+                        sb.Append(IsUppder ? chars[i].ToString().ToUpper() : chars[i].ToString().ToLower());
+                        IsUppder = false;
+                        break;
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ CreateTableEntities/CreateTableEntities/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CreateTableEntities
+{
+    /// <summary>
+    /// Dtoクラス生成アプリケーション
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = GetSchema().GroupBy(row => row["TableName"]);
+
+            foreach(var table in list)
+            {
+                var tableName = table.Key.ToString();
+                var builder = new EntityBuilder();
+
+                builder.AddTable(tableName);
+
+                foreach (var row in table)
+                {
+                    builder.AddField(
+                        row["DataType"].ToString(),
+                        row["ColumnName"].ToString()
+                        );
+                }
+
+                _ = WriteFileAsync(
+                    builder.TableName, 
+                    builder.Build()
+                    );
+            }
+        }
+
+        private static IEnumerable<DataRow> GetSchema()
+        {
+            var conStr = ConfigurationManager.ConnectionStrings["Wakabadai"].ConnectionString;
+            var dt = new DataTable();
+
+            using (var connection = new SqlConnection(conStr))
+            {
+                connection.Open();
+
+                new SqlDataAdapter(@"
+                                    SELECT
+                                         t.name                  AS TableName
+                                        ,c.name                  AS ColumnName
+                                        ,type_name(user_type_id) AS DataType
+                                        ,max_length              AS Length
+                                        ,is_nullable             AS Nullable
+                                    FROM
+                                        sys.objects t
+                                        INNER JOIN sys.columns c ON t.object_id = c.object_id
+                                    WHERE
+                                        t.type = 'U'
+                                    ORDER BY
+                                        t.name",
+                                        connection)
+                    .Fill(dt);
+            }
+
+            return dt.AsEnumerable();
+        }
+
+        private async static Task WriteFileAsync(string tableName, string text)
+        {
+            var folderPath = ConfigurationManager.AppSettings["FolderPath"];
+
+            var fullPath = Path.Combine(
+                string.IsNullOrEmpty(folderPath) 
+                    ? Environment.CurrentDirectory 
+                    : folderPath,
+                $"{tableName}.cs"
+                );
+
+            using (var sw = new StreamWriter(fullPath, false))
+                await sw.WriteAsync(text).ConfigureAwait(false);
+        }
+    }
+}
